88f3b08429232d77b58baee629efaf9e4b020a0f
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-07-11 21:51:59 +0300

    Add project files.

---
+++ sGrades/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sGrades.Startup))]
+namespace sGrades
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
